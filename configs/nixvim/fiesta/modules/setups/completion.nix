9c06ae5be79b671dde0825ac879d7784f148e512
Gabriel Arazas <foodogsquared@foodogsquared.one>
2025-06-16 17:20:11 +0800

    nixvimConfigs/fiesta/setups/completion: migrate to blink.cmp

--- configs/nixvim/fiesta/modules/setups/completion.nix
+++ configs/nixvim/fiesta/modules/setups/completion.nix
@@ -9,53 +9,25 @@ in {
 
   config = lib.mkIf cfg.enable (lib.mkMerge [
     {
-      plugins.cmp = {
+      plugins.blink-cmp = {
         enable = true;
-        autoEnableSources = true;
+        settings = {
+          completion = {
+            accept = {
+              auto_brackets = {
+                enabled = true;
+                semantic_token_resolution.enabled = true;
+              };
+            };
 
-        settings.mapping = {
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-u>" = "cmp.mapping.scroll_docs(4)";
-          "<C-g>" = "cmp.mapping.close()";
-          "<C-n>" = "cmp.mapping.select_next_item()";
-          "<C-p>" = "cmp.mapping.select_prev_item()";
-          "<Tab>" = "cmp.mapping.select_next_item()";
-          "<S-Tab>" = "cmp.mapping.select_prev_item()";
+            documentation.auto_show = true;
+          };
         };
-
-        settings.sources =
-          [ { name = "nvim_lsp"; } { name = "path"; } { name = "buffer"; } ];
       };
-
-      # All of the typical completion sources I would need.
-      plugins.cmp-buffer.enable = true;
-      plugins.cmp-path.enable = true;
-      plugins.cmp-vim-lsp.enable = true;
-      plugins.cmp-nvim-lua.enable = true;
     }
 
-    (lib.mkIf nixvimCfg.setups.debugging.enable {
-      plugins.cmp-dap.enable = true;
-    })
-
-    (lib.mkIf nixvimCfg.setups.snippets.enable {
-      plugins.cmp.settings = {
-        snippet.expand = ''
-          function(args)
-            require('luasnip').lsp_expand(args.body)
-          end
-        '';
-
-        sources = [{ name = "luasnip"; }];
-      };
-
-      plugins.cmp_luasnip.enable = true;
-    })
-
-    (lib.mkIf nixvimCfg.setups.treesitter.enable {
-      plugins.cmp-treesitter.enable = true;
+    (lib.mkIf config.nixvimConfigs.fiesta.setups.snippets.enable {
+      plugins.blink-cmp.settings.snippets.preset = "luasnip";
     })
   ]);
 }
