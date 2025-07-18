62ac08361920532409d5714f74eb0ebd93456dac
Gabriel Arazas <foodogsquared@foodogsquared.one>
2025-07-18 22:08:07 +0800

    hosts/ni/services/backup: remove desktop backup
    This is mostly expected to be set per-user now through Borgmatic or
    whatever works.

--- configs/nixos/ni/modules/services/backup/default.nix
+++ configs/nixos/ni/modules/services/backup/default.nix
@@ -5,7 +5,7 @@ let
   hostCfg = config.hosts.ni;
   cfg = hostCfg.services.backup;
 
-  borgJobCommonSetting = { patterns ? [ ], passCommand, ... }@args:
+  borgJobCommonSetting = { patterns ? [ ], patternFiles ? [ ], passCommand, ... }@args:
     let args' = lib.attrsets.removeAttrs args [ "patterns" "passCommand" ];
     in {
       compression = "zstd,12";
@@ -16,9 +16,10 @@ let
         mode = "repokey-blake2";
       };
       extraCreateArgs = let
-        patternsFromArgs = lib.map (patternFile: "--patterns-from ${patternFile}") patterns;
+        patternsFromArgs = lib.map (patternFile: "--patterns-from ${patternFile}") patternFiles;
+        patternArgs = lib.map (pattern: "--pattern ${pattern}") patterns;
       in lib.concatStringsSep " "
-        (patternsFromArgs ++ [
+        (patternsFromArgs ++ patternArgs ++ [
           "--exclude-if-present .nobackup"
           "--stats"
         ]);
@@ -51,44 +52,20 @@ in {
   config = lib.mkIf cfg.enable {
     sops.secrets = foodogsquaredLib.sops-nix.getSecrets ./secrets.yaml
       (foodogsquaredLib.sops-nix.attachSopsPathPrefix pathPrefix {
-        "patterns/home" = { };
-        "patterns/root" = { };
         "patterns/keys" = { };
-        "repos/archives/password" = { };
         "repos/external-hdd/password" = { };
-        "repos/hetzner-box/password" = { };
-        "repos/hetzner-box/ssh-key" = { };
       });
 
     suites.filesystem.setups = { laptop-ssd.enable = true; };
 
     services.borgbackup.jobs = {
       local-external-storage = borgJobCommonSetting {
-        patterns = with config.sops; [
-          secrets."${pathPrefix}/patterns/root".path
-          secrets."${pathPrefix}/patterns/keys".path
-        ];
         passCommand = "cat ${
             config.sops.secrets."${pathPrefix}/repos/external-hdd/password".path
           }";
         removableDevice = true;
         doInit = true;
         repo = "${config.state.paths.laptop-ssd}/Backups";
-      };
-
-      remote-backup-hetzner-box = borgJobCommonSetting {
-        patterns = with config.sops;
-          [ secrets."${pathPrefix}/patterns/home".path ];
-        passCommand = "cat ${
-            config.sops.secrets."${pathPrefix}/repos/hetzner-box/password".path
-          }";
-        doInit = true;
-        repo =
-          "ssh://${hetzner-boxes-user}@${hetzner-boxes-server}:23/./borg/desktop/ni";
-        startAt = "04:30";
-        environment.BORG_RSH = "ssh -i ${
-            config.sops.secrets."${pathPrefix}/repos/hetzner-box/ssh-key".path
-          }";
       };
     };
 
