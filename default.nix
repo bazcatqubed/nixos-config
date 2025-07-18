05c2169dc69a68e94bdc214d815f3901ebe4f535
Gabriel Arazas <foodogsquared@foodogsquared.one>
2025-07-18 22:09:02 +0800

    default.nix: remove
    It is not really worth to have this through anymore.

--- default.nix
+++
@@ -1,8 +0,0 @@
-{ compat-flake ? "flake-compat-fds" }:
-
-(import (let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-in fetchTarball {
-  url =
-    lock.nodes.${compat-flake}.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-  sha256 = lock.nodes.${compat-flake}.locked.narHash;
-}) { src = ./.; }).defaultNix
