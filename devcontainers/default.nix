96dcbc3f71f4b689ae5204297c6aba0ea72cbaf2
Gabriel Arazas <foodogsquared@foodogsquared.one>
2025-11-04 14:56:16 +0800

    devcontainers: remove Ruby 3.2 and add Ruby 3.5

--- devcontainers/default.nix
+++ devcontainers/default.nix
@@ -6,7 +6,7 @@ in {
   typicalDevenv = callPackage ./typical-devenv.nix { };
   rustBackend = callPackage ./rust-backend.nix { };
   jsBackend = callPackage ./js-backend.nix { };
-  ruby_3_2 = callPackage ./ruby-on-rails.nix { ruby = pkgs.ruby_3_2; };
   ruby_3_3 = callPackage ./ruby-on-rails.nix { ruby = pkgs.ruby_3_3; };
   ruby_3_4 = callPackage ./ruby-on-rails.nix { ruby = pkgs.ruby_3_4; };
+  ruby_3_5 = callPackage ./ruby-on-rails.nix { ruby = pkgs.ruby_3_5; };
 }
