b8462d26586b4645080097a479422d9543b0e353
Gabriel Arazas <foodogsquared@foodogsquared.one>
2026-01-26 20:08:04 +0800

    users/foo-dogsquared/programs/nushell: update completion script

--- configs/home-manager/foo-dogsquared/modules/programs/nushell.nix
+++ configs/home-manager/foo-dogsquared/modules/programs/nushell.nix
@@ -61,7 +61,7 @@ in
             let external_completers = {|spans|
               let expanded_alias = scope aliases
                 | where name == $spans.0
-                | get -i 0.expansion
+                | get --optional 0.expansion
 
               let spans = if $expanded_alias != null {
                 $spans
