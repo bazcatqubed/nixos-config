9e30762e5a15cf69e47f6e65e96d74ca291bfccf
Gabriel Arazas <foodogsquared@foodogsquared.one>
2026-04-09 09:55:43 +0800

    lib/env-specific/nixvim: add `mkPrefixBinding` and `mkPrefixBindingFunc`

--- lib/env-specific/nixvim.nix
+++ lib/env-specific/nixvim.nix
@@ -8,6 +8,16 @@
   self,
 }:
 
-{
+rec {
   isStandalone = config: !config ? hmConfig && !config ? nixosConfig && !config ? darwinConfig;
+
+  mkPrefixBindingFunc =
+    { prefix, ... }@attrs:
+    binding: settings:
+    lib.mergeAttrs {
+      mode = attrs.mode or "n";
+      key = "${prefix}${binding}";
+    } settings;
+
+  mkPrefixBinding = attrs: lib.mapAttrsToList (mkPrefixBindingFunc attrs);
 }
