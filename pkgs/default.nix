ca7714834331646124b059f302afe20ee8bb5be4
Gabriel Arazas <foodogsquared@foodogsquared.one>
2026-04-15 12:47:05 +0800

    pkgs: update callPackage function

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,89 +9,99 @@
 let
   inherit (pkgs)
     lib
-    callPackage
     python3Packages
     qt5
+    qt6
     ;
 in
-lib.makeScope pkgs.newScope (self: {
-  # My custom nixpkgs extensions.
-  foodogsquaredLib = import ../lib { inherit pkgs; };
-  inherit (self.foodogsquaredLib.builders)
-    makeXDGMimeAssociationList
-    makeXDGPortalConfiguration
-    makeXDGDesktopEntry
-    buildHugoSite
-    buildMdbookSite
-    buildZolaSite
-    buildMkdocsSite
-    buildAntoraSite
-    buildFDSEnv
-    buildDconfDb
-    buildDconfProfile
-    buildDconfConf
-    buildDconfPackage
-    buildDockerImage
-    buildBlenderAddons
-    ;
-  inherit (self.foodogsquaredLib.fetchers)
-    fetchInternetArchive
-    fetchUgeeDriver
-    fetchWebsiteIcon
-    fetchPexelsImages
-    fetchPexelsVideos
-    fetchUnsplashImages
-    ;
+lib.makeScope pkgs.newScope (
+  self:
+  let
+    callPackage = pkgs.newScope self;
+  in
+  {
+    # My custom nixpkgs extensions.
+    foodogsquaredLib = import ../lib { inherit pkgs; };
+    inherit (self.foodogsquaredLib.builders)
+      makeXDGMimeAssociationList
+      makeXDGPortalConfiguration
+      makeXDGDesktopEntry
+      buildHugoSite
+      buildMdbookSite
+      buildZolaSite
+      buildMkdocsSite
+      buildAntoraSite
+      buildFDSEnv
+      buildDconfDb
+      buildDconfProfile
+      buildDconfConf
+      buildDconfPackage
+      buildDockerImage
+      buildBlenderAddons
+      buildSuperColliderQuark
+      ;
+    inherit (self.foodogsquaredLib.fetchers)
+      fetchInternetArchive
+      fetchUgeeDriver
+      fetchWebsiteIcon
+      fetchPexelsImages
+      fetchPexelsVideos
+      fetchUnsplashImages
+      fetchSupercolliderQuark
+      ;
 
-  # My custom packages.
-  awesome-cli = callPackage ./awesome-cli { };
-  bobby = callPackage ./bobby { };
-  base16-builder-go = callPackage ./base16-builder-go { };
-  blender-blendergis = python3Packages.callPackage ./blender-blendergis { };
-  blender-machin3tools = python3Packages.callPackage ./blender-machin3tools { };
-  blender-cad-sketcher = pkgs.python314Packages.callPackage ./blender-cad-sketcher { };
-  colour-science = python3Packages.callPackage ./colour-science { };
-  clidle = callPackage ./clidle.nix { };
-  ctrld = callPackage ./ctrld { };
-  domterm = qt5.callPackage ./domterm { };
-  durdraw = python3Packages.callPackage ./durdraw { };
-  fastn = callPackage ./fastn { };
-  flatsync = callPackage ./flatsync { };
-  freerct = callPackage ./freerct.nix { };
-  gnome-search-provider-recoll = callPackage ./gnome-search-provider-recoll.nix { };
-  #graphite-design-tool = callPackage ./graphite-design-tool { };
-  go-avahi-cname = callPackage ./go-avahi-cname { };
-  grant = callPackage ./grant { };
-  hush-shell = callPackage ./hush-shell.nix { };
-  kip = callPackage ./kip { };
-  lektra = callPackage ./lektra { };
-  lwp = callPackage ./lwp { };
-  moac = callPackage ./moac.nix { };
-  mopidy-beets = callPackage ./mopidy-beets.nix { };
-  mopidy-funkwhale = callPackage ./mopidy-funkwhale.nix { };
-  mopidy-internetarchive = callPackage ./mopidy-internetarchive.nix { };
-  mopidy-listenbrainz = callPackage ./mopidy-listenbrainz { };
-  nautilus-annotations = callPackage ./nautilus-annotations { };
-  pop-launcher-plugin-brightness = callPackage ./pop-launcher-plugin-brightness { };
-  pop-launcher-plugin-duckduckgo-bangs = callPackage ./pop-launcher-plugin-duckduckgo-bangs.nix { };
-  pop-launcher-plugin-jetbrains = callPackage ./pop-launcher-plugin-jetbrains { };
-  pigeon-mail = callPackage ./pigeon-mail { };
-  swh = callPackage ./software-heritage {
-    python3Packages = pkgs.python312Packages;
-  };
-  #pd-l2ork = callPackage ./pd-l2ork { };
-  #rotp-modnar = callPackage ./rotp-modnar { };
-  #rotp-fusion = callPackage ./rotp-fusion { };
-  #purrdata = callPackage ./purr-data { };
-  speki = callPackage ./speki { };
-  sqlc-gen-from-template = callPackage ./sqlc-gen-from-template { };
-  simple-icons = callPackage ./simple-icons { };
-  tic-80-unstable = callPackage ./tic-80 { };
-  sessiond = callPackage ./sessiond { };
-  vgc = qt5.callPackage ./vgc { };
-  watc = callPackage ./watc { };
-  willow = callPackage ./willow { };
-  wzmach = callPackage ./wzmach { };
-  xs = callPackage ./xs { };
-  yolk = callPackage ./yolk { };
-})
+    # My custom packages.
+    awesome-cli = callPackage ./awesome-cli { };
+    bobby = callPackage ./bobby { };
+    base16-builder-go = callPackage ./base16-builder-go { };
+    blender-blendergis = python3Packages.callPackage ./blender-blendergis { };
+    blender-machin3tools = python3Packages.callPackage ./blender-machin3tools { };
+    blender-cad-sketcher = pkgs.python314Packages.callPackage ./blender-cad-sketcher { };
+    colour-science = python3Packages.callPackage ./colour-science { };
+    clidle = callPackage ./clidle.nix { };
+    ctrld = callPackage ./ctrld { };
+    domterm = qt6.callPackage ./domterm { };
+    durdraw = python3Packages.callPackage ./durdraw { };
+    fastn = callPackage ./fastn { };
+    flatsync = callPackage ./flatsync { };
+    freerct = callPackage ./freerct.nix { };
+    gnome-search-provider-recoll = callPackage ./gnome-search-provider-recoll.nix { };
+    #graphite-design-tool = callPackage ./graphite-design-tool { };
+    go-avahi-cname = callPackage ./go-avahi-cname { };
+    grant = callPackage ./grant { };
+    hush-shell = callPackage ./hush-shell.nix { };
+    kip = callPackage ./kip { };
+    lektra = callPackage ./lektra { };
+    lwp = callPackage ./lwp { };
+    moac = callPackage ./moac.nix { };
+    mopidy-beets = callPackage ./mopidy-beets.nix { };
+    mopidy-funkwhale = callPackage ./mopidy-funkwhale.nix { };
+    mopidy-internetarchive = callPackage ./mopidy-internetarchive.nix { };
+    mopidy-listenbrainz = callPackage ./mopidy-listenbrainz { };
+    nautilus-annotations = callPackage ./nautilus-annotations { };
+    # nautilus-custom-icon-name = callPackage ./nautilus-icon-name { };
+    pop-launcher-plugin-brightness = callPackage ./pop-launcher-plugin-brightness { };
+    pop-launcher-plugin-duckduckgo-bangs = callPackage ./pop-launcher-plugin-duckduckgo-bangs.nix { };
+    pop-launcher-plugin-jetbrains = callPackage ./pop-launcher-plugin-jetbrains { };
+    pigeon-mail = callPackage ./pigeon-mail { };
+    swh = callPackage ./software-heritage {
+      python3Packages = pkgs.python312Packages;
+    };
+    #pd-l2ork = callPackage ./pd-l2ork { };
+    #rotp-modnar = callPackage ./rotp-modnar { };
+    #rotp-fusion = callPackage ./rotp-fusion { };
+    #purrdata = callPackage ./purr-data { };
+    # reshade = callPackage ./reshade { };
+    speki = callPackage ./speki { };
+    sqlc-gen-from-template = callPackage ./sqlc-gen-from-template { };
+    simple-icons = callPackage ./simple-icons { };
+    tic-80-unstable = callPackage ./tic-80 { };
+    sessiond = callPackage ./sessiond { };
+    vgc = qt5.callPackage ./vgc { };
+    watc = callPackage ./watc { };
+    willow = callPackage ./willow { };
+    wzmach = callPackage ./wzmach { };
+    xs = callPackage ./xs { };
+    yolk = callPackage ./yolk { };
+  }
+)
