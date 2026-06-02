51e04b6e3c934b4b07016a69043aa05ca53eafd9
Gabriel Arazas <foodogsquared@foodogsquared.one>
2026-06-02 16:50:14 +0800

    nixos/profiles/desktop: add workaround for crashing Qt apps on file openers with GTK-based desktop portals

--- modules/nixos/profiles/desktop/default.nix
+++ modules/nixos/profiles/desktop/default.nix
@@ -5,10 +5,20 @@
 # A common profile for desktop systems. Most of the configurations featured
 # here should be enough in common to the typical desktop setups found on
 # non-NixOS systems.
+{ pkgs, lib, ... }:
+
 {
   imports = [
     ./fonts.nix
     ./audio.nix
     ./hardware.nix
+  ];
+
+  # Workaround for crashing file openers with GTK-based desktop portals on QT
+  # apps since nixpkgs has weird wrapping process for them.
+  #
+  # For more information, see NixOS/nixpkgs#149812.
+  environment.sessionVariables.XDG_DATA_DIRS = [
+    "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
   ];
 }
